f951173774251aa5219ab41a00b5f682747d85b4
Julianes04113 <96156554+Julianes04113@users.noreply.github.com>
2022-01-04 09:53:24 -0500

    Creacion de rutas
    Se crean las rutas y se modifica el .env para indicar al 127.0.0.1:8000 como rama main

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('auth/login');
-});
+Route::get('/', function () {return view('auth/login');})->name('main');
 
 Auth::routes();
 
 Auth::routes(['verify' => true]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('products', function () {
+    return 'Esta es la lista de productos';})->name('products.index');
+
+Route::get('products/create', function () {
+    return 'Este es el formulario para crear un producto';})->name('products.create');
+
+Route::post('products', function () {
+    })->name('products.store');
+
+Route::get('products/{product}', function ($product){ 
+    return "Mostrando el producto con ID {$product}";})->name('products.show');
+Route::get('products/{product}/edit', function ($product){ 
+    return "Mostrando el formulario para editar el producto con ID {$product}";})->name('products.edit');
+Route::match(['put','patch'], 'products/{product}', function ($product){ 
+    ;})->name('products.update');
+
+Route::delete('products/{product}', function ($product){ 
+    ;})->name('products.destroy');
