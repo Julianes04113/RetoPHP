dde2e7e5f0785514bb3eeec46920c74f0a6f6fe7
Julianes04113 <96156554+Julianes04113@users.noreply.github.com>
2022-01-26 21:06:35 -0500

    Fix: Tailwind CSS compatibility Issue's with Bootstrap
    Fix: Desintalling  Bootstrap CSS and Reinstalling Tailwind CSS about compatibility issues between them
    Feat: Improving Create Product form

--- resources/views/Products/create.blade.php
+++ resources/views/Products/create.blade.php
@@ -1,3 +1,11 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -13,38 +21,41 @@
 
                     <form method="POST" action="{{route('products.store') }}">
                            @csrf
-                           <div class="form-row">
-                                <label>Titulo</label>
-                                <input class="form-control" type="text" name="title" required>
-                            </div>
-                            <div class="form-row">
-                                <label>Descripción</label>
-                                <input class="form-control" type="text" name="description" required>
-                            </div>
-                            <div class="form-row">
-                                <label>Precio</label>
-                                <input class="form-control" type="number" min="1" name="price" required>
-                            </div>
-                            <div class="form-row">
-                                <label>Cantidad</label>
-                                <input class="form-control" type="number" min="0" name="stock" required>
-                            </div>
-                            <div class="form-row">
-                                <label>Estado</label>
-                                <select class="custom-select" name="status" required>
-                                    <option value="" selected>Seleccionar...</option>
-                                    <option value="unavailable" >Disponible</option>
-                                    <option value="unavailable">No disponible</option>
-                            </div>
-                            
-                              
-                    </form>
-                    <p>aklgo</p>
-                      <div><button>Crear Producto</button>
-                               <!-- <button class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded" type="submit">Crear Producto</button> //Tailwind -->
-                           </div>
+                      <!-- Titulo -->
+<div class="flex">
+    <span class="text-sm border border-2 rounded-l px-4 py-2 bg-gray-300 whitespace-no-wrap">Nombre:</span>
+    <input name="field_name" class="border border-2 rounded-r px-4 py-2 w-full" type="text" placeholder="Ingrese el nombre del producto" />
+</div>
+                         <!-- Descripción -->
+<div class="flex">
+    <span class="text-sm border border-2 rounded-l px-4 py-2 bg-gray-300 whitespace-no-wrap">Descripción:</span>
+    <input name="field_name" class="border border-2 rounded-r px-4 py-2 w-full" type="text" placeholder="Ingrese el precio del producto" />
+</div>
+<!--Precio -->
+<div class="flex">
+    <span class="text-sm border border-2 rounded-l px-4 py-2 bg-gray-300 whitespace-no-wrap">Precio:</span>
+    <input name="field_name" class="border border-2 rounded-r px-4 py-2 w-full" type="number"/>
+</div>
+<!-- Cantidad-->
+<div class="flex">
+    <span class="text-sm border border-2 rounded-l px-4 py-2 bg-gray-300 whitespace-no-wrap">Cantidad:</span>
+    <input name="field_name" class="border border-2 rounded-r px-4 py-2 w-full" type="number"/>
+</div>
+                
+<!--Stock-->
+<div class="w-full flex flex-col mb-3">
+<label class="font-semibold text-gray-800 py-2">Stock</label>
+<select class="block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-4 md:w-full " required>
+<option value="Available">Disponible</option>
+<option value="Unavailable">No Disponible</option></select>
+<button class="bg-yellow-100 hover:bg-blue-500 text-black font-bold py-2 px-4 rounded">Crear Producto</button>
+</div>
+
+                        </form>
                 </div>
             </div>
         </div>
     </div>
 </x-app-layout>
+</body>
+</html>
